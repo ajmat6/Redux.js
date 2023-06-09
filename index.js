eb5ea6b16e4e342b7028924d49ab392d51e40a7f
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-09 18:42:54 +0530

    Implementing many actions in one reducer to handle more than one initial state properties

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const redux = require('redux') // importing the redux library
 const createStore = redux.legacy_createStore // to create store
+const bindActionCreators = redux.bindActionCreators // For binding the action creators into an object of action creators which will be used in store.dispatch function
 
 // Type of the action is generally a string constraint:
-const BUY_CAKE = 'BUT_CAKE';
+const BUY_CAKE = 'BUY_CAKE';
 const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+const BUY_ICECREAM = 'BUY_ICECREAM';
+const ICECREAM_RESTOCKED = 'ICECREAM_RESTOCKED';
 
 // Implementing Action Creators:
 
@@ -11,8 +15,7 @@ function buyCake () // action creator function that returns an action
 {
     return {
             type: BUY_CAKE, // this is an action of type BUY_CAKE
-            info: 'First Redux action',
-            quantity: 1
+            payload: 1
         }     
 }
 
@@ -20,8 +23,23 @@ function restockCake(qty=1)
 {
     return {
         type: CAKE_RESTOCKED,
-        info: 'Action of cake restocking',
-        quantity: qty
+        payload: qty
+    }
+}
+
+function buyIcecream () // action creator function that returns an action
+{
+    return {
+            type: BUY_ICECREAM, // this is an action of type BUY_CAKE
+            payload: 1
+        }     
+}
+
+function restockIcecream(qty=1)
+{
+    return {
+        type: ICECREAM_RESTOCKED,
+        payload: qty
     }
 }
 
@@ -29,7 +47,8 @@ function restockCake(qty=1)
 
 // Defining initial state according to the first principle of Redux:
 const initialState = {
-    noOfCakes: 10
+    noOfCakes: 10,
+    noOfIcecreams: 20
 }
 
 const reducer = (state = initialState, action) => {
@@ -41,8 +60,18 @@ const reducer = (state = initialState, action) => {
 
         case CAKE_RESTOCKED: return {
             ...state,
-            noOfCakes: state.noOfCakes + action.quantity // it means you are stocking as much cakes as you pass in the quantity
+            noOfCakes: state.noOfCakes + action.payload // it means you are stocking as much cakes as you pass in the quantity
         }
+
+        case BUY_ICECREAM: return {
+            ...state, 
+            noOfIcecreams: state.noOfIcecreams - 1
+        }
+
+        case ICECREAM_RESTOCKED: return {
+            ...state,
+            noOfIcecreams: state.noOfIcecreams + action.payload
+        } 
 
         default: return state
     }
@@ -58,11 +87,21 @@ const unsubscribe = store.subscribe(() => // Resgistering listeners via subscrib
     console.log('Updated state', store.getState())
 ) 
 
-store.dispatch(buyCake()) // Updating the state via dispatch which accepts an action as a parameter (3rd responsibility of the redux store)
-store.dispatch(buyCake()) // Updating the state three times
-store.dispatch(buyCake())
+// store.dispatch(buyCake()) // Updating the state via dispatch which accepts an action as a parameter (3rd responsibility of the redux store)
+// store.dispatch(buyCake()) // Updating the state three times
+// store.dispatch(buyCake())
 
-store.dispatch(restockCake(3)); // restocking the cakes by 3
+// store.dispatch(restockCake(3)); // restocking the cakes by 3
+
+// using bind action creators to update the store(it is same as above):
+const actions = bindActionCreators({buyCake, restockCake, buyIcecream, restockIcecream}, store.dispatch);
+actions.buyCake();
+actions.buyCake();
+actions.buyCake();
+actions.restockCake(3);
+
+actions.buyIcecream();
+actions.restockIcecream(5);
 
 unsubscribe(); // 5th responsibility of the redux store (unsubsribe to any changes in the store and after this if you change any values in the state it will not be listened)
 
