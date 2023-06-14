e21df71678e1888a7b8f82744be3dd8bec9f3c82
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-14 20:58:42 +0530

    useSelector hook in react redux

--- react-reduxtoolkit-project/src/features/cake/CakeView.jsx
+++ react-reduxtoolkit-project/src/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux' // importing useSelector to access any state in redux store
 
 function CakeView() {
+    const noOfCakes = useSelector((state) => state.cake.noOfCakes) // useSelector accepts redux state as an argument and it returns a value(here is cake reducer's noOfCakes value from redux store state)
   return (
     <div>
-      <h2>Number of Cakes - </h2>
+      <h2>Number of Cakes - {noOfCakes}</h2>
       <button>Order Cake</button>
       <button>Restock Cake</button>
     </div>
--- react-reduxtoolkit-project/src/features/icecream/IcecreamView.jsx
+++ react-reduxtoolkit-project/src/features/icecream/IcecreamView.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 
 function IcecreamView() {
+    const noOfIcecreams = useSelector((state) => state.icecream.noOfIcecreams)
   return (
     <div>
-      <h2>Number of Icecreams - </h2>
+      <h2>Number of Icecreams - {noOfIcecreams}</h2>
       <button>Order Icecream</button>
       <button>Restock Icecream</button>
     </div>
