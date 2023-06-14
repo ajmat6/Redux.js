ae8e1bd71e293646f9f68aee0d95a6158a9761bf
ajmat6 <114720514+ajmat6@users.noreply.github.com>
2023-06-14 21:26:09 +0530

    useDispatch hook in react redux

--- react-reduxtoolkit-project/src/features/cake/CakeView.jsx
+++ react-reduxtoolkit-project/src/features/cake/CakeView.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import { useSelector } from 'react-redux' // importing useSelector to access any state in redux store
+import React, { useState } from 'react'
+import { useSelector, useDispatch } from 'react-redux' // importing useSelector to access any state in redux store
+import { ordered, restocked } from './cakeSlice' // importing actions
 
 function CakeView() {
+    const [value, setvalue] = useState(1)
     const noOfCakes = useSelector((state) => state.cake.noOfCakes) // useSelector accepts redux state as an argument and it returns a value(here is cake reducer's noOfCakes value from redux store state)
+    const dispatch = useDispatch(); // to use useDispatch storing it in a variable
   return (
     <div>
       <h2>Number of Cakes - {noOfCakes}</h2>
-      <button>Order Cake</button>
-      <button>Restock Cake</button>
+      <button onClick={() => dispatch(ordered())}>Order Cake</button>
+      {/* Passing 5 as a payload in the restocked action */}
+      <input type="number" value={value} onChange={(e) => setvalue(parseInt(e.target.value))} />
+      <button onClick={() => dispatch(restocked(value))}>Restock Cake</button> 
     </div>
   )
 }
--- react-reduxtoolkit-project/src/features/icecream/IcecreamView.jsx
+++ react-reduxtoolkit-project/src/features/icecream/IcecreamView.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
+import { ordered, restocked } from './icecreamSlice';
 
 function IcecreamView() {
     const noOfIcecreams = useSelector((state) => state.icecream.noOfIcecreams)
+    const dispatch = useDispatch();
   return (
     <div>
       <h2>Number of Icecreams - {noOfIcecreams}</h2>
-      <button>Order Icecream</button>
-      <button>Restock Icecream</button>
+      <button onClick={() => dispatch(ordered())}>Order Icecream</button>
+      <button onClick={() => dispatch(restocked(10))}>Restock Icecream</button>
     </div>
   )
 }
